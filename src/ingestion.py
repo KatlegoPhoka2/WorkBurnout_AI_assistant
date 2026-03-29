94c31b857295d28909b6e92d873a2d7067562386
KatlegoPhoka2 <katlegophoka6@gmail.com>
2026-03-29 12:08:06 +0200

    load data

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,27 +2,29 @@
 
 from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader,PyPDFDirectoryLoader
 
+# Loading data 
+
 def load_pdf_documents():
         
-  pdf_loader = PyPDFDirectoryLoader("path")
+  pdf_loader = PyPDFLoader("data/documents/workbrnout.pdf")
   pdf_docs = pdf_loader.load()
   return pdf_docs
 
 
 def load_web_documents():
-  
-  web_loader = WebBaseLoader("https://example.com")
-  web_docs = web_loader.load()
-  return web_docs
+    urls = [
+        "https://www.deloitte.com/southeast-asia/en/about/people/blogs/mental-health-at-work.html",
+        "https://www.who.int/news-room/fact-sheets/detail/mental-health-at-work"
+    ]
+    
+    web_loader = WebBaseLoader(urls)
+    web_docs = web_loader.load()
+    
+    return web_docs
 
-pdf_docs = load_pdf_documents("data/pdfs/")
-
-web_docs = load_web_documents([
-    "https://example.com",
-    "https://another-site.com"
-])
-
-all_docs = pdf_docs + web_docs
+pdf_docs=load_pdf_documents()
+web_docs =load_web_documents()
+all_docs =pdf_docs+ web_docs
 
 print(f"Total documents: {len(all_docs)}")
 
